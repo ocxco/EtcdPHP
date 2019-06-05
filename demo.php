70d25237739c63eac1560732235c6c58e2ae404d
xuechaoc <chenxc@stargoto.com>
2019-06-05 14:20:23 +0800

    保持跟etcdctl api 命名以及效果一致

--- demo.php
+++ demo.php
@@ -11,17 +11,25 @@ require __DIR__ . '/vendor/autoload.php';
 
 $client = \EtcdPHP\clients\Client::instance();
 
-//$a1 = $client->get('a1');
-//
-//var_dump($a1);
+/**
+ * Test Key
+ */
+$a1 = $client->mk('a123', 123456);
+$a2 = $client->get('a123');
+$a3 = $client->set('a123', '11111');
+$a4 = $client->update('a123', '12345');
+$a5 = $client->rm('a123');
+var_dump($a1, $a2, $a3, $a4, $a5);
 
-//$res = $client->getNode('a3');
-//var_dump($res->node);
-//
-//
-//$res = $client->listDir('/');
-//var_dump($res->node);
-//$client->setRoot('3e3e');
-//$res = $client->add('a1', '132');
-$res = $client->listDir('/', true);
-var_dump($res->action, $res->node);
+/**
+ * Test Dir
+ */
+$a1 = $client->mkdir('dirA');
+$a2 = $client->set('dirA/aaaa', 'aaaa');
+$a3 = $client->ls('dirA');
+$a4 = $client->updateDir('dirA', 200);
+$a5 = $client->ls('dirA');
+$a6 = $client->setDir('dirA', 100);
+$a7 = $client->ls('dirA');
+$a8 = $client->rmdir('dirA', true);
+var_dump($a1, $a2, $a3, $a4, $a5, $a6, $a7, $a8);
--- src/clients/Client.php
+++ src/clients/Client.php
@@ -2,187 +2,8 @@
 
 namespace EtcdPHP\clients;
 
-use GuzzleHttp\Client as gzClient;
-use GuzzleHttp\Exception\BadResponseException;
-use Psr\Http\Message\ResponseInterface;
-
-class Client
+class Client extends Base
 {
-    /**
-     * @var gzClient
-     */
-    public $http;
-
-    /**
-     * @var string storeRoot
-     */
-    public $root = '';
-
-    /**
-     * @var string etcdServer
-     */
-    public $server;
-
-    /**
-     * @var string etcdApiVersion
-     */
-    public $apiVersion;
-
-    /**
-     * @var self
-     */
-    private static $_instance;
-
-    public static function instance($server = 'http://localhost:2379', $apiVersion = 'v2')
-    {
-        if (!self::$_instance instanceof self) {
-            self::$_instance = new self();
-        }
-        self::$_instance->server = $server;
-        self::$_instance->apiVersion = $apiVersion;
-        self::$_instance->http = new gzClient([
-            'base_uri' => self::$_instance->server
-        ]);
-        return self::$_instance;
-    }
-
-    /**
-     * 解析返回的数据.
-     * @param ResponseInterface $response
-     * @return Response
-     *
-     * @throws
-     */
-    private function result(ResponseInterface $response)
-    {
-        $body = json_decode($response->getBody(), true);
-        if (JSON_ERROR_NONE !== json_last_error()) {
-            throw new \Exception("Response error to parse");
-        }
-        $resp = Response::instance();
-        $resp->setAttributes($body);
-        if ($resp->errorCode) {
-            throw new \Exception($resp->message, $resp->errorCode);
-        }
-        return $resp;
-    }
-
-    /**
-     * 根据key获取Api路径.
-     * @param $key
-     * @return string
-     */
-    protected function getKeyPath($key)
-    {
-        if (strpos($key, '/') !== 0) {
-            $key = '/' . $key;
-        }
-        $path = "/{$this->apiVersion}/keys{$this->root}{$key}";
-        return $path;
-    }
-
-    /**
-     * 根据key获取value
-     * @param $key
-     * @param array $params
-     * @throws
-     *
-     * @return Response
-     */
-    private function get($key, $params = [])
-    {
-        try {
-            $response = $this->http->get($this->getKeyPath($key), ['query' => $params]);
-        } catch (BadResponseException $e) {
-            $response = $e->getResponse();
-        }
-        $body = $this->result($response);
-        return $body;
-    }
-
-    /**
-     * 设置一个值.
-     * @param $key
-     * @param $value
-     * @param $dir
-     * @param null $ttl
-     * @param array $cond
-     * @return Response
-     *
-     * @throws
-     */
-    private function set($key, $value = null, $dir = null, $ttl = null, $cond = [])
-    {
-        $data = [];
-        if ($value) {
-            $data['value'] = $value;
-        } elseif ($dir) {
-            $data['dir'] = 'true';
-        }
-        if ($ttl) {
-            $data['ttl'] = $ttl;
-        }
-
-        try {
-            $response = $this->http->put($this->getKeyPath($key), [
-                'query' => $cond,
-                'form_params' => $data
-            ]);
-        } catch (BadResponseException $e) {
-            $response = $e->getResponse();
-        }
-        $body = $this->result($response);
-        return $body;
-    }
-
-    /**
-     * 删除一个key
-     * @param $key
-     * @param null $dir
-     * @param bool $recursive
-     * @return Response
-     *
-     * @throws
-     */
-    private function delete($key, $dir = null, $recursive = false)
-    {
-        $cond = [];
-        if ($dir) {
-            $data['dir'] = 'true';
-        }
-        if ($recursive) {
-            $data['recursive'] = 'true';
-        }
-
-        try {
-            $response = $this->http->delete($this->getKeyPath($key), [
-                'query' => $cond,
-            ]);
-        } catch (BadResponseException $e) {
-            $response = $e->getResponse();
-        }
-        $body = $this->result($response);
-        return $body;
-    }
-
-    /**
-     * 设置存储根路径.
-     * @param $root
-     * @return $this
-     */
-    public function setRoot($root)
-    {
-        if (strpos($root, '/') !== 0) {
-            $root = '/' . $root;
-        }
-        $this->root = rtrim($root, '/');
-        return $this;
-    }
-
-    public function getNode($key, $params = [])
-    {
-        return $this->get($key, $params);
-    }
 
     /**
      * 新增一个Key
@@ -192,10 +13,117 @@ class Client
      * @return Response
      * @throws \Exception
      */
-    public function add($key, $value, $ttl = 0)
+    public function mk($key, $value, $ttl = 0)
     {
-        $body = $this->set($key, $value, null, $ttl, ['prevExist' => 'false']);
+        $body = $this->_set($key, $value, null, $ttl, ['prevExist' => 'false']);
         return $body;
+    }
+
+    /**
+     * 新建一个文件夹.
+     * @param $key
+     * @param int $ttl
+     * @return Response
+     * @throws \Exception
+     */
+    public function mkdir($key, $ttl = 0)
+    {
+        $body = $this->_set($key, null, true, $ttl, [
+            'prevExist' => 'false'
+        ]);
+        return $body;
+    }
+
+    /**
+     * 删除一个key.
+     * @param $key
+     * @return Response
+     */
+    public function rm($key)
+    {
+        $body = $this->_delete($key);
+        return $body;
+    }
+
+    /**
+     * 删除dir.
+     * @param $key
+     * @param bool $recursive
+     * @return Response
+     */
+    public function rmdir($key, $recursive = false)
+    {
+        $body = $this->_delete($key, true, $recursive);
+        return $body;
+    }
+
+    /**
+     * @param $key
+     * @param array $params
+     * @return Response
+     */
+    public function get($key, $params = [])
+    {
+        return $this->_get($key, $params);
+    }
+
+    /**
+     * 列表.
+     * @param string $key 开始的路径.
+     * @param bool $recursive 是否递归.
+     * @return mixed
+     */
+    public function ls($key = '/', $recursive = false)
+    {
+        $query = [];
+        if ($recursive === true) {
+            $query['recursive'] = 'true';
+        }
+        $body = $this->get($key, $query);
+        return $body;
+    }
+
+    /**
+     * 设置一个key，不存在则创建，存在则更新.
+     * @param $key
+     * @param $value
+     * @return Response
+     * @throws \Exception
+     */
+    public function set($key, $value)
+    {
+        try {
+            $res = $this->mk($key, $value);
+        } catch (\Exception $e) {
+            if (105 == $e->getCode()) {
+                // key已经存在.
+                $res = $this->update($key, $value);
+            } else {
+                throw $e;
+            }
+        }
+        return $res;
+    }
+
+    /**
+     * 新建或者更新一个dir
+     * @param $key
+     * @param $ttl
+     * @return Response
+     * @throws \Exception
+     */
+    public function setDir($key, $ttl)
+    {
+        try {
+            $res = $this->mkdir($key, $ttl);
+        } catch (\Exception $e) {
+            if (105 == $e->getCode()) {
+                $res = $this->updateDir($key, $ttl);
+            } else {
+                throw $e;
+            }
+        }
+        return $res;
     }
 
     /**
@@ -209,49 +137,7 @@ class Client
     public function update($key, $value, $ttl = 0, $condition = [])
     {
         $condition['prevExist'] = 'true';
-        $body = $this->set($key, $value, null, $ttl, $condition);
-        return $body;
-    }
-
-    /**
-     * 删除一个key.
-     * @param $key
-     * @return Response
-     */
-    public function rm($key)
-    {
-        $body = $this->delete($key);
-        return $body;
-    }
-
-    /**
-     * 新建一个文件夹.
-     * @param $key
-     * @param int $ttl
-     * @return Response
-     * @throws \Exception
-     */
-    public function mkdir($key, $ttl = 0)
-    {
-        $body = $this->set($key, null, true, $ttl, [
-            'prevExist' => 'false'
-        ]);
-        return $body;
-    }
-
-    /**
-     * 列表.
-     * @param string $key 开始的路径.
-     * @param bool $recursive 是否递归.
-     * @return mixed
-     */
-    public function listDir($key = '/', $recursive = false)
-    {
-        $query = [];
-        if ($recursive === true) {
-            $query['recursive'] = 'true';
-        }
-        $body = $this->get($key, $query);
+        $body = $this->_set($key, $value, null, $ttl, $condition);
         return $body;
     }
 
@@ -264,26 +150,11 @@ class Client
      */
     public function updateDir($key, $ttl = 0)
     {
-        if (!$ttl) {
-            throw new \Exception("TTL is required", 204);
-        }
         $cond = [
             'dir' => 'true',
             'prevExist' => 'true',
         ];
-        $body = $this->set($key, null, true, $ttl, $cond);
-        return $body;
-    }
-
-    /**
-     * 删除dir.
-     * @param $key
-     * @param bool $recursive
-     * @return Response
-     */
-    public function rmdir($key, $recursive = false)
-    {
-        $body = $this->delete($key, true, $recursive);
+        $body = $this->_set($key, null, true, $ttl, $cond);
         return $body;
     }
 
